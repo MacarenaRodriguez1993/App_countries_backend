6a35c40b61a5f0f91567d7625046b2a73d47f05a
Macarena Rodriguez <maca.b.rodriguez@gmail.com>
2022-11-24 14:57:31 -0300

    conexion

--- src/db.js
+++ src/db.js
@@ -35,7 +35,7 @@ let sequelize=
       ssl:true,
     })
     : new Sequelize(
-      `postgres://${DB_USER}:${DB_PASSWORD}@${DB_HOST}:${DB_PORT}/countries`,
+      `postgres://${DB_USER}:${DB_PASSWORD}@${DB_HOST}/countries`,
       {
         logging:false,
         native:false
